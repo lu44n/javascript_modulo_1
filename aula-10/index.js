af7f076f4b88386303989238437e7c8e8f005652
Mikael <99888430+lu44n@users.noreply.github.com>
2024-03-19 13:05:58 -0300

    Código Aula10

---
+++ aula-10/index.js
@@ -0,0 +1,16 @@
+let nome = "Mikael"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Insira o seu nome:")
+//let confirmacao = window.confirm("Confirma essa ação?")
+
+//window é o objeto global do navegador
+//alert("Olá, mundo!")
+//prompt("Olá")
+//confirm("Confirma?")
+
+//document faz referência a página HTML
